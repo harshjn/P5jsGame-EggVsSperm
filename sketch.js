caa8fba9dfdb3cac21cff86d211152fb624617af
Harsh Jain <harshjainldh@gmail.com>
2020-11-29 01:42:48 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ function setup() {
 
   flock = new Flock();
   // Add an initial set of boids into the system
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 100; i++) {
     let b = new Boid(width / 2,height / 2);
     flock.addBoid(b);
   }
